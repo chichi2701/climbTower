9ee4d232da0415485934d283e8f991b8f92d1476
Thanh Dat - Unity Dev <dattt@AS.LOCAL>
2025-10-21 13:33:38 +0700

    init

---
+++ climbTower/Assets/GAME/Scripts/Helplers/EventDefine.Game.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public partial class EventDefine {
+    // ─────────────────────────────────────────
+    //  GAME EVENTS
+    // ─────────────────────────────────────────
+
+    public struct OnChangeStateJoyStick : IEventParam { public bool IsChange; }
+    public struct OnTimeEnd : IEventParam { }
+    public struct OnUpdateProgress : IEventParam { }
+    public struct OnKilled : IEventParam { public GameObject KilledCharacter; }
+    public struct OnChangeSkin : IEventParam { public int skinID; }
+    public struct OnFinishGameSession : IEventParam { }
+    public struct OnFinishBoosterGift : IEventParam { }
+    public struct OnFinishPopup : IEventParam { public string PopupName; public bool IsRewardFinsh; };
+    public struct OnUpdateBoosterDisplay : IEventParam { }
+
+}
